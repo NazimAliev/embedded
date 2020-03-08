6879880d004df02dbbd483bc88d85dd388a946c7
legolas <tnemo@mail.com>
2020-03-08 18:33:27 +0300

    wrong user

--- embedded/voip-gateway-arm/ftsip/version.h
+++
@@ -1,1 +0,0 @@
-static char const rcsid[] = "$Revision: 1.03-r415:484M $";
--- embedded/voip-gateway-arm/ftsip/version.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo -n 'static char const rcsid[] = "$Revision: 1.03-r' > version.h
-svnversion -n . >> version.h
-echo -n ' $";' >> version.h
-echo >> version.h
